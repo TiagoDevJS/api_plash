91fa4c752192c630f8e7b185dc51211ee2179627
TiagoDevJS <tiaguinhorb2010@gmail.com>
2024-09-27 07:59:49 -0300

    Doc delte employee

--- src/UseCases/Employees/deleteEmployeeUseCase.ts
+++ src/UseCases/Employees/deleteEmployeeUseCase.ts
@@ -1,32 +1,53 @@
-import { inject, singleton } from "tsyringe/dist/decorators";
+import { inject, injectable, singleton } from "tsyringe/dist/decorators";
 import { DeleteEmployeUseCaseInterface } from "../../Interfaces/Employees/deleteEmployee";
 import { EmployeeRepository } from "../../Repositories/Employees";
 import logger from "../../adapters/winstomLogger";
 
- @singleton()
-export class EmployeeUSeCaseDelete  implements DeleteEmployeUseCaseInterface {
-   constructor(
+/**
+ * @class EmployeeUSeCaseDelete
+ * @implements {DeleteEmployeUseCaseInterface}
+ * @description Caso de uso responsável por deletar um colaborador (Employee) no sistema.
+ */
+@injectable()
+export class EmployeeUSeCaseDelete implements DeleteEmployeUseCaseInterface {
+  /**
+   * @constructor
+   * @description Inicializa o caso de uso com o repositório necessário para deletar colaboradores.
+   * 
+   * @param {EmployeeRepository} employeeRepo - Repositório responsável por manipular dados dos colaboradores.
+   */
+  constructor(
     //@ts-ignore
-     @inject(EmployeeRepository)
-     private readonly employeeRepo: EmployeeRepository
-   ){
+    @inject(EmployeeRepository)
+    private readonly employeeRepo: EmployeeRepository
+  ) {}
 
-   }
-   async execute(id: number): Promise<boolean> {
-    logger.info(`Ìniciando servico deletar colaborador `)
-      const existEmployee = await this.employeeRepo.findID(id)
-      if(!existEmployee){
-        logger.warn(`Colaborador ${id} não encontrado `)
-        return false
-      }
-      try {
-        await this.employeeRepo.delete(id)
-        logger.info(`Colaborador   deletado com sucesso! `)
-        return true
-      } catch (error) {
-        logger.info(`Erro servico deletar colaborador ${error} `)
-        return false
-      }
-      
-   }
+  /**
+   * @method execute
+   * @description Executa o processo de deleção de um colaborador no sistema, verificando se o colaborador existe antes de removê-lo.
+   * 
+   * @param {number} id - ID do colaborador que será deletado.
+   * @returns {Promise<boolean>} Retorna `true` se o colaborador for deletado com sucesso, ou `false` caso contrário.
+   */
+  async execute(id: number): Promise<boolean> {
+    logger.info(`Iniciando serviço para deletar colaborador`);
+
+    // Verifica se o colaborador com o ID existe no sistema
+    const existEmployee = await this.employeeRepo.findID(id);
+
+    if (!existEmployee) {
+      logger.warn(`Colaborador ${id} não encontrado`);
+      return false;
+    }
+
+    try {
+      // Deleta o colaborador do repositório
+      await this.employeeRepo.delete(id);
+      logger.info(`Colaborador deletado com sucesso!`);
+      return true;
+    } catch (error) {
+      logger.error(`Erro no serviço ao deletar colaborador: ${error}`);
+      return false;
+    }
+  }
 }
