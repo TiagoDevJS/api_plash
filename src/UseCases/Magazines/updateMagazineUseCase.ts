15d5ca1f6201751fae7db937d6e28fee9a10ba6f
TiagoDevJS <tiaguinhorb2010@gmail.com>
2024-09-27 08:43:08 -0300

    Doc Update mgazines

--- src/UseCases/Magazines/updateMagazineUseCase.ts
+++ src/UseCases/Magazines/updateMagazineUseCase.ts
@@ -1,4 +1,4 @@
-import { inject, singleton } from "tsyringe/dist/decorators";
+import { inject, injectable, singleton } from "tsyringe/dist/decorators";
 import { Magazine } from "../../Entities/magazine";
 import { MagazineUpdateInterface } from "../../Interfaces/Magazines/UpdateMagazineInterface";
 import { MagazineRepository } from "../../Repositories/Magazines";
@@ -6,32 +6,49 @@ import logger from "../../adapters/winstomLogger";
 import { BadRequestError, NotFoundError } from "../../handleError/errors";
 
 
-// Camada de serviço responsavel por enviar   dados e manipular para atualizar uma revistas.
-@singleton()
-export class MagazineUseCaseUpdate implements MagazineUpdateInterface{
+/**
+ * @class MagazineUseCaseUpdate
+ * @description Caso de uso responsável por atualizar as informações de uma revista no sistema.
+ */
+@injectable()
+export class MagazineUseCaseUpdate implements MagazineUpdateInterface {
+  /**
+   * @constructor
+   * @description Injeta o repositório de revistas necessário para realizar a atualização das revistas.
+   * 
+   * @param {MagazineRepository} magazineRepo - Repositório responsável pela manipulação dos dados das revistas.
+   */
   constructor(
     //@ts-ignore
-@inject(MagazineRepository)
-private readonly magazineRepo:MagazineRepository
-  ){
+    @inject(MagazineRepository)
+    private readonly magazineRepo: MagazineRepository
+  ) {}
 
-  }
-   // Funçao responsavel por enviar os dados para atualizar  revistas no banco de dados
+  /**
+   * @method execute
+   * @description Atualiza as informações de uma revista existente com base no ID fornecido e nos dados de entrada.
+   * 
+   * @param {number} id - ID da revista a ser atualizada.
+   * @param {Magazine} input - Dados atualizados da revista.
+   * @returns {Promise<string | null>} Retorna uma mensagem de sucesso ou null se não for encontrado.
+   * 
+   * @throws {NotFoundError} Caso a revista não seja encontrada.
+   * @throws {BadRequestError} Caso ocorra um erro ao chamar o repositório de revistas.
+   */
   async execute(id: number, input: Magazine): Promise<string | null> {
-       const existMagazine = await this.magazineRepo.findID(id)
-       if(!existMagazine){
-        logger.warn(`Revista não encontrada`)
-        throw new NotFoundError('Revista não encontrada')
-       }
+    const existMagazine = await this.magazineRepo.findID(id);
+    if (!existMagazine) {
+      logger.warn(`Revista não encontrada`);
+      throw new NotFoundError("Revista não encontrada");
+    }
 
     try {
-      const data = Magazine.create(input)
-      const update = await this.magazineRepo.update(existMagazine.id, data)
-      return update
+      const data = Magazine.create(input);
+      const update = await this.magazineRepo.update(existMagazine.id, data);
+      return update;
     } catch (error) {
-      logger.error(`Erro use case ${error}`)
-      throw new BadRequestError('Erro ao chamar respositorio de revistas')
+      logger.error(`Erro use case ${error}`);
+      throw new BadRequestError("Erro ao chamar repositório de revistas");
     }
-   
   }
 }
