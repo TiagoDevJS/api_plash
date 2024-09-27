07c5304fc82a6851386096319284e7c3e6886baa
TiagoDevJS <tiaguinhorb2010@gmail.com>
2024-09-27 07:43:17 -0300

    Doc get dvls

--- src/UseCases/Dvls/getDvlUseCase.ts
+++ src/UseCases/Dvls/getDvlUseCase.ts
@@ -3,45 +3,61 @@ import { GetDvlUseCaseIntrface } from "../../Interfaces/Dvls/GetDvlUseCaseInterf
 import { DvlsRepository } from "../../Repositories/Dvls";
 import logger from "../../adapters/winstomLogger";
 import { BadRequestError, NotFoundError } from "../../handleError/errors";
-// Class GET usada para buscar dados do dvls no sistema
+
+/**
+ * @class GET UseCase DVL
+ * @description Classe responsável por listar os dvl.
+ */
 @injectable()
 export class GetDvlUseCase implements GetDvlUseCaseIntrface {
-    constructor(
-        //@ts-ignore
-        @inject(DvlsRepository)
-        private readonly dvlsRepo:DvlsRepository
-    ){}
-    //Funçao que busca os dados no respository e retorna uma lista de dvl 
-    async findAll(): Promise<any> {
-        try {
-            // Faz a chamada para o respository e retorna uma array de dvls
-            const data = await this.dvlsRepo.findAll()
-            // Retorna os dados da chamada
-            return data
-            
-        } catch (error:any) {
-            logger.error(`Erro ao buscar dvls no banco de dados ${error.message}`)
-            throw new BadRequestError(`Erro ao buscar vls na base de dados `)
-        }
+  constructor(
+    //@ts-ignore
+    @inject(DvlsRepository)
+    private readonly dvlsRepo: DvlsRepository
+  ) {}
+  /**
+    
+   * @description Executa a função para listar os DVLS.
+   * @returns Retorna uma lista de dvls array 
+   * @throws {BadRequestError} Lança um erro caso a ocorra alguma  falha.
+
+    */
+  async findAll(): Promise<any> {
+    try {
+     
+      const data = await this.dvlsRepo.findAll();
+     
+      return data;
+    } catch (error: any) {
+      logger.error(`Erro ao buscar dvls no banco de dados ${error.message}`);
+      throw new BadRequestError(`Erro ao buscar vls na base de dados `);
     }
-     //Funçao que busca os dados no respository e retorna um objeto de dvl 
-    async  findID(id: number): Promise<any> {
-        // Busca o dvl no respository
-        const existDvl = await this.dvlsRepo.findID(id)
-        // Verifica se existe caso contrario lança um erro nao encontrado
-        if(!existDvl){
-            // Logger para verificar qual id nao foi encontrado 
-            logger.warn(`Dvl ${id} não encontrado!`)
-            throw new NotFoundError(`Dvl não encontrado`)
-        }
-        try {
-        // Retorna os dados encontrado 
-            return existDvl
-        } catch (error:any) {
-            // Exibe o log de erro 
-            logger.error(`Erro ao buscar dvl solicitado :${error.message}`)
-            //Retorna badquest para outro tipo de erro 
-            throw new BadRequestError(`Erro ao buscar dvl solicitado`)
-        }
+  }
+   /**
+    
+   * @description Executa a função para listar um DVL.
+   * @returns Retorna uma objeto de dvl
+   *  @throws {NotFoundError} Lança um erro caso não encontre o dvl buscado.
+   * @throws {BadRequestError} Lança um erro caso a ocorra alguma  falha.
+
+    */
+  async findID(id: number): Promise<any> {
+  
+    const existDvl = await this.dvlsRepo.findID(id);
+   
+    if (!existDvl) {
+     
+      logger.warn(`Dvl ${id} não encontrado!`);
+      throw new NotFoundError(`Dvl não encontrado`);
     }
+    try {
+     
+      return existDvl;
+    } catch (error: any) {
+      
+      logger.error(`Erro ao buscar dvl solicitado :${error.message}`);
+      
+      throw new BadRequestError(`Erro ao buscar dvl solicitado`);
+    }
+  }
 }
